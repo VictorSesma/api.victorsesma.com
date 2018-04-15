4ee062a47a6ee4b8bab5db4664aa91145bee9bd6
leviatan89 <leviatan.89@gmail.com>
2018-04-15 12:54:34 +0100

    Readme added

---
+++ README.md
@@ -0,0 +1,1 @@
+Go victorsesma.com Repository
--- api.go
+++ api.go
@@ -34,6 +34,7 @@ func cvHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 	// fmt.Fprintf(w, ) prints to the browser
 }
